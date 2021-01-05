9a5843b78c460d34086b33c3674b234779a6d850
WesleyZhen <wzhen@ucsd.edu>
2021-01-04 21:39:49 -0800

    hotfix: group_comments
    got rid of deque used dictionary instead of deque so i can have instant access

--- IGBotToolkit.py
+++ IGBotToolkit.py
@@ -5,7 +5,6 @@ from CountDownConsole import countdown
 from random import randint
 from time import sleep
 from datetime import datetime
-from collections import deque
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -538,20 +537,20 @@ class InstagramBotTools:
 
             if group_by is None:
                 group_by_dict = dict()
-                group_by_dict["comments"] = deque([])
-                for comment in comment_list:
+                group_by_dict["comments"] = dict()
+                for i, comment in enumerate(comment_list):
                     # the user name
                     user = comment.find_element_by_css_selector("a[class='sqdOP yWX7d     _8A5w5   ZIAjV ']").text
 
                     # the comment text
                     comment_text = comment.find_element_by_css_selector("span[class='']").text
-                    
-                    group_by_dict["comments"].append((user, comment_text))
+
+                    group_by_dict["comments"][i] = (user, comment_text)
                 return group_by_dict
             
             if group_by == 'user':
                 group_by_dict = dict()
-                for comment in comment_list:
+                for i, comment in enumerate(comment_list):
                     # the user name
                     user = comment.find_element_by_css_selector("a[class='sqdOP yWX7d     _8A5w5   ZIAjV ']").text
 
@@ -559,13 +558,13 @@ class InstagramBotTools:
                     comment_text = comment.find_element_by_css_selector("span[class='']").text
 
                     if user not in group_by_dict:
-                        group_by_dict[user] = deque([])
-                    group_by_dict[user].append(comment_text)
+                        group_by_dict[user] = dict()
+                    group_by_dict[user][i] = comment_text
                 return group_by_dict
             
             if group_by == 'full_date':
                 group_by_dict = dict()
-                for comment in comment_list:
+                for i, comment in enumerate(comment_list):
                     f_date = comment.find_element_by_css_selector("time[class='FH9sR Nzb55']").get_attribute('datetime')
                     f_date_lis = f_date.split('-')
                     
@@ -582,9 +581,9 @@ class InstagramBotTools:
                     if month not in group_by_dict[year]:
                         group_by_dict[year][month] = dict()
                     if day not in group_by_dict[year][month]:
-                        group_by_dict[year][month][day] = deque([])
+                        group_by_dict[year][month][day] = dict()
 
-                    group_by_dict[year][month][day].append(comment_text)
+                    group_by_dict[year][month][day][i] = comment_text
 
                 return group_by_dict
 
